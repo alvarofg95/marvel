f372dd0087e9ac2b0607c28e243d75a5aa958d41
alvarofg95 <alvarofg95dev@gmail.com>
2022-08-26 00:07:25 +0200

    fix errorpage styles

--- src/components/errorPage/index.js
+++ src/components/errorPage/index.js
@@ -3,7 +3,7 @@ import "./index.css";
 
 const ErrorPage = () => {
   return (
-    <div>
+    <div className="errorPage">
       <h1>Oops, an error has ocurred, please, reload the page</h1>
     </div>
   );
